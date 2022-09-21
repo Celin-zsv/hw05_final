49a968a651ab8f0c6b23fbd729ccb08c871963f3
Сергей Зеленковский <svzelenkoskii@gmail.com>
2022-09-21 15:30:26 +0300

    проект спринта-6, ver.1, Зеленковский Сергей

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import (get_object_or_404,
-                              redirect, render)
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.cache import cache_page
 
 from .forms import CommentForm, PostForm
